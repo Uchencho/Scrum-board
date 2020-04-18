f05a819ae8eee674f361a34c1b8e35704907b4d6
Uchencho <aloziekelechi17@gmail.com>
2020-04-18 12:10:53 +0100

    started working on angular

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from .api import ListApi, CardApi
 from django.urls import path, include
@@ -7,4 +8,5 @@ from django.urls import path, include
 urlpatterns = [
     path('lists/', ListApi.as_view()),
     path('card/', CardApi.as_view()),
+    path('home/', TemplateView.as_view(template_name='scrumboard/home.html')),
 ]
